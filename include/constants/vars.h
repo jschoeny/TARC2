fc8f83569272cd24b3212132fa1520ba574e37aa
Jared Schoeny <jared@schoeny.com>
2025-09-29 16:50:46 -1000

    Finish scripting after defeating guardians

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -279,6 +279,8 @@ enum StoryProgress
     STORY_RETURNED_AFTER_WOOPER,
     STORY_ENTERED_ROCKY_PATH,
     STORY_DEFEATED_GUARDIANS,
+    STORY_RETURNED_AFTER_GUARDIANS,
+    STORY_ENTERED_MOUNTAIN_SUMMIT,
 };
 #define VAR_PASSIVE_HEAL_COUNTER                         0x40F8
 #define VAR_UNUSED_0x40F9                                0x40F9 // Unused Var
